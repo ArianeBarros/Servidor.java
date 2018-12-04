fa3a2f11bb701dd7df6f79b7e2440f85a2716453
LorennaNunes <42344195+LorennaNunes@users.noreply.github.com>
2018-12-04 13:06:35 -0200

    po

--- controle/enviaveis/Mensagem.java
+++ controle/enviaveis/Mensagem.java
@@ -1,32 +1,66 @@
 package controle.enviaveis;
 
+//import java.net.*;
+//import java.util.ArrayList;
+
 public class Mensagem implements Enviavel
 {
-	private String texto;
-	private String destinatario;
+	protected String texto;
+	protected String destinatario;
 
-	public Mensagem(Enviavel enviar)
+	public void setTexto(String t)
 	{
-      this.texto = enviar.toString();
+		this.texto = t;
 	}
 
-	public void envia()
+	public void setDestinatario(String d)
 	{
-
+		this.destinatario = d;
 	}
 
-	/*public int hashCode()
+	public String getTexto()
 	{
+		return this.texto;
+	}
+
+	public String getDestinatario()
+	{
+		return this.destinatario;
+	}
+
+	public String toString()
+	{
+		return this.texto + this.destinatario;
+	}
+
+	public int hashCode()
+	{
+		int ret = 3;
+		ret = ret * 7 + new String(this.destinatario).hashCode();
+		ret = ret * 7 + new String(this.texto).hashCode();;
+
+		return ret;
 	}
 
 	public boolean equals(Object obj)
 	{
 		if(obj == null)
 			return false;
+
 		if(this == obj)
 			return true;
+
 		if(this.getClass()!= obj.getClass())
 			return false;
+
+		Mensagem msg = (Mensagem)obj;
+
+		if(!this.texto.equals(msg.texto))
+			return false;
+
+		if(!this.destinatario.equals(msg.destinatario))
+			return false;
+
 		return true;
-	}*/
+	}
 }
